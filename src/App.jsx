0438a648b5cb31bb65c0c680b32552e8f599c8f8
Onion <ahorn103@gmx.de>
2024-11-08 14:51:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainCard from './components/MainCard.jsx';
+import Navbar from './components/Navbar.jsx';
+
+const App = () => (
+  <div id="container">
+    <Navbar />
+    <MainCard />
+  </div>
+);
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div id="headerTitle">
+      LocalOnionSeller
+      <hr className="border" style={{ width: '80%' }} />
+    </div>
+  );
+};
+
+export default Navbar;
